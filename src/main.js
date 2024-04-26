1a562d5782fcaaf2edf4c4310e0f45d10578f475
jlruedam <jlruedam@gmail.com>
2024-04-25 22:52:25 -0500

    16-Lista de películas recomendadas.

--- src/main.js
+++ src/main.js
@@ -130,7 +130,7 @@ async function getMovieById(id) {
 
 }
 async function getRelatedMoviesId(id){
-    const {data} = await api('/movie/'+ id + "/recommendations");
+    const {data} = await api('/movie/'+ id + "/similar");
     const relatedMovies = data.results;
 
     createMovies(relatedMovies,relatedMoviesContainer);
